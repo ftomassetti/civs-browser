2172e11c53a4bc5dc7dd107f978cd0bca6fbb0a6
Federico Tomassetti <f.tomassetti@gmail.com>
2014-08-15 19:53:56 +0100

    Skeleton of web app in place

--- project.clj
+++ project.clj
@@ -1,7 +1,19 @@
 (defproject civs-browser "0.1.0-SNAPSHOT"
   :description "A web application to visualize the history files produced by civs"
   :url "https://github.com/ftomassetti/civs-browser"
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [
+                  [org.clojure/clojure "1.6.0"]
+                  [civs "0.1.0-SNAPSHOT"]
+                  ;[hiccup "1.0.5"]
+                  [javax.servlet/servlet-api "2.5"]
+                  [ring/ring-core "1.3.0"]
+                  [ring/ring-devel "1.3.0"]
+                  [ring/ring-jetty-adapter "1.3.0"]
+                  [compojure "1.1.8"]
+                ]
+  :dev-dependencies
+                [[lein-run "1.0.1-SNAPSHOT"]]
   :main ^:skip-aot civs-browser.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :repositories {"sonartype snapshots" "https://oss.sonatype.org/content/repositories/snapshots"})
--- src/civs_browser/core.clj
+++ src/civs_browser/core.clj
@@ -1,7 +1,32 @@
 (ns civs-browser.core
+  (:use compojure.core)
+  (:use hiccup.core)
+  (:use hiccup.page)
+  (:use ring.adapter.jetty)
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn view-layout [& content]
+  (html
+    (doctype :xhtml-strict)
+    (xhtml-tag "en"
+      [:head
+       [:meta {:http-equiv "Content-type"
+               :content "text/html; charset=utf-8"}]
+       [:title "Civs-Browser"]]
+      [:body content])))
+
+(defn homepage []
+  (view-layout
+    [:h1 "Welcome to Civs-Browser!"]))
+
+(defn parse-input [a b]
+  [(Integer/parseInt a) (Integer/parseInt b)])
+
+(defroutes app
+  (GET "/" []
+    (homepage)))
+
+(defn -main [& args]
+
+  (run-jetty #'civs-browser.core/app {:port 8080}))
+
