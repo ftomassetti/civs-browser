006c9b86c00afea0de983c1647361f0dd39a7548
Federico Tomassetti <f.tomassetti@gmail.com>
2014-08-25 22:40:01 +0100

    Testing model turns

--- test/civs_browser/model_test.clj
+++ test/civs_browser/model_test.clj
@@ -5,9 +5,35 @@
 
 (def my-history (load-history-file-fressian "examples-history/w77_100turns.history" "examples-worlds"))
 
+;#########################################
+; Turns
+;#########################################
+
 (deftest test-n-turns
   (is (= 100 (n-turns my-history))))
+
+(deftest test-turns
+  (is (= (range 101) (turns my-history))))
+
+(deftest test-exist-turn
+  (is (= true (exist-turn? my-history 0)))
+  (is (= true (exist-turn? my-history 100)))
+  (is (= true (exist-turn? my-history 50)))
+  (is (= false (exist-turn? my-history -1)))
+  (is (= false (exist-turn? my-history 101))))
+
+;#########################################
+; Game
+;#########################################
 
 (deftest test-exist-group?
   (is (= true (exist-group? my-history 1)))
   (is (= false (exist-group? my-history 123456))))
+
+;#########################################
+; World
+;#########################################
+
+;#########################################
+; Groups
+;#########################################
